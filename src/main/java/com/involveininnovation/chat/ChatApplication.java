c7d4914e408275bd4e02e6dd5b3ccd894eaba4e8
Rudrakumar Patel <rudrapatel2992003@gmail.com>
2024-06-10 19:00:08 +0530

    update

--- src/main/java/com/involveininnovation/chat/ChatApplication.java
+++ src/main/java/com/involveininnovation/chat/ChatApplication.java
@@ -1,11 +1,18 @@
 package com.involveininnovation.chat;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChatApplication {
 	public static void main(String[] args){
+
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("spring.datasource.url", dotenv.get("DATABASE_URL"));
+		System.setProperty("spring.datasource.username", dotenv.get("DATABASE_USER"));
+		System.setProperty("spring.datasource.password", dotenv.get("DATABASE_PASSWORD"));
+
 		SpringApplication.run(ChatApplication.class, args);
 	}
 }
